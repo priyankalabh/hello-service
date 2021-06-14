735d51d84ddea59ffdc9b6dd863d436f50bfa7e8
priyanka <labh.priyanka@gmail.com>
2021-06-14 10:32:27 +0200

    Hello service Gin implementation

--- main.go
+++ main.go
@@ -1,43 +1,20 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/api/hello", helloHandler)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Listen and serve error  %v", err)
-	}
+	router := gin.Default()
+	router.GET("/api/hello", helloHandler)
+	router.Run(":5050")
 
 }
 
-type helloResponse struct {
-	Message string `json:"message"`
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-	hello := helloResponse{
-		Message: "Hello",
-	}
-
-	data, err := json.Marshal(&hello)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	_, err = w.Write(data)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	return
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "hello",
+	})
 }
