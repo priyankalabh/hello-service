d274d83c296a40911d75f2b68cbf3281aebd7216
priyanka <labh.priyanka@gmail.com>
2021-06-20 13:56:22 +0200

    make port configurable through the command line.

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	"hello-service/router"
 	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	port string
+)
+
+func init() {
+	flag.StringVar(&port, "port", ":5050", "this is a REST server port")
+}
+
 func main() {
+	flag.Parse()
 	server := gin.Default()
 	router.AttachRouter(server)
-	err := server.Run(":5050")
+	err := server.Run(port)
 	if err != nil {
 		log.Fatalf("Run error: %v", err)
 	}
